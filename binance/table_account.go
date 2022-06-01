b8d2962162a99eb3fb203d20bed59bd6dabf8748
YG Park <ygpark80@gmail.com>
2022-06-01 17:27:05 +0900

    type (Spot/Flexible) added to binance_account

--- binance/table_account.go
+++ binance/table_account.go
@@ -2,6 +2,7 @@ package binance
 
 import (
 	"context"
+	"strings"
 
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/plugin"
@@ -15,8 +16,9 @@ func tableBinanceAccount(ctx context.Context) *plugin.Table {
 		},
 		Columns: []*plugin.Column{
 			{Name: "asset", Type: proto.ColumnType_STRING},
-			{Name: "free", Type: proto.ColumnType_STRING},
-			{Name: "locked", Type: proto.ColumnType_STRING},
+			{Name: "type", Type: proto.ColumnType_STRING},
+			{Name: "free", Type: proto.ColumnType_DOUBLE},
+			{Name: "locked", Type: proto.ColumnType_DOUBLE},
 		},
 	}
 }
@@ -26,8 +28,49 @@ func listAccount(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData
 	res, _ := client.NewGetAccountService().Do(ctx)
 
 	for _, t := range res.Balances {
-		d.StreamListItem(ctx, t)
+		d.StreamListItem(ctx, Balance{
+			Asset:  getAsset(t.Asset),
+			Type:   getType(t.Asset),
+			Free:   t.Free,
+			Locked: t.Locked,
+		})
 	}
 
 	return nil, nil
+}
+
+func getAsset(asset string) string {
+	if asset == "LDBAKE" {
+		return "QTUM"
+	}
+	if asset == "LDBAKET" {
+		return "BAKE"
+	}
+	if asset == "LDERD" {
+		return "EGLD"
+	}
+	if asset == "LDLEND" {
+		return "TKO"
+	}
+	if asset == "LDSHIB2" {
+		return "SHIB"
+	}
+	if getType(asset) == "Flexible" {
+		return asset[2:]
+	}
+	return asset
+}
+
+func getType(asset string) string {
+	if strings.HasPrefix(asset, "LD") && asset != "LDO" {
+		return "Flexible"
+	}
+	return "Spot"
+}
+
+type Balance struct {
+	Asset  string
+	Type   string
+	Free   string
+	Locked string
 }
